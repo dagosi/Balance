8b8ee591a06b3bbc5a46645e903127bec2ab79cb
Daniel Gomez Sierra <danielgomezsierra@gmail.com>
2012-11-08 11:27:12 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Balance::Application.config.secret_token = 'bcedcd3c2966943b861cc7fdb606fd513459b4db977bc7b0bd5e2f2b3beb0e51cb93f043882e7efa9835ed4b5a12ec48617c406f3d7d5a479cf1e9b69ef67304'
