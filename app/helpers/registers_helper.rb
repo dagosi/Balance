ab7d879db27950f32929067e8060950182852907
Daniel Gomez Sierra <danielgomezsierra@gmail.com>
2012-11-12 20:31:35 -0500

    Added select to balance type.

--- app/helpers/registers_helper.rb
+++ app/helpers/registers_helper.rb
@@ -1,2 +1,9 @@
 module RegistersHelper
+  def show_paid_status(register)
+    if register.balance_type == 'Incoming'
+      return ''
+    else
+      return register.paid
+    end
+  end
 end
--- app/models/register.rb
+++ app/models/register.rb
@@ -1,14 +1,13 @@
 class Register < ActiveRecord::Base
   belongs_to :user
   attr_accessible :amount, :balance_type, :date, :description, :paid
-  attr_reader :BALANCE_TYPE
 
   validates :amount, presence: true
   validates :description, presence: true
   validates :balance_type, presence: true
   validates :date, presence: true
 
-  BALANCE_TYPE = 
-    [ { name: 'Incoming', id: 1 }, { name: 'Outgoing', id: 2 }]
+  BALANCE_TYPE = ['Incoming', 'Outgoing']
+  
 
 end
