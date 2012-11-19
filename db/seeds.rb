eee9890b42bc8713065f21afc4508333f8682be0
Daniel Gomez Sierra <danielgomezsierra@gmail.com>
2012-11-19 04:17:52 -0500

    Added seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 def create_users
+  
   # Creates an admin.
   admin = User.create(email: 'admin@admin.com', password: 'admin1234', admin: true)
-
+  
   if admin.errors.any?
     puts "There are errors on admin creation."
   else
@@ -17,67 +18,67 @@ def create_users
     puts "Daniel was created successfully."
   end
 end      
-     
+
 # Creates several registers for both users.
 def create_registers
   admin = User.first
   
   # January
   admin.registers.create(date: '2012-01-01', description: 'Rent', amount: 1000, 
-                        balance_type: 'Outgoing')
+                         balance_type: 'Outgoing')
   
   admin.registers.create(date: '2012-01-01', description: 'Gas', amount: 150, 
-                        balance_type: 'Outgoing')
+                         balance_type: 'Outgoing')
 
   admin.registers.create(date: '2012-01-01', description: 'Food', amount: 300, 
-                        balance_type: 'Outgoing')
+                         balance_type: 'Outgoing')
   
   admin.registers.create(date: '2012-01-01', description: 'Subway', amount: 100, 
-                        balance_type: 'Outgoing')
+                         balance_type: 'Outgoing')
   
   admin.registers.create(date: '2012-01-01', description: 'Income', amount: 3000, 
-                        balance_type: 'Incoming')
+                         balance_type: 'Incoming')
   
   admin.registers.create(date: '2012-01-01', description: 'Page income', amount: 1000, 
-                        balance_type: 'Incoming')  
+                         balance_type: 'Incoming')  
 
   # February
   admin.registers.create(date: '2012-02-01', description: 'Rent', amount: 1500, 
-                        balance_type: 'Outgoing')
+                         balance_type: 'Outgoing')
   
   admin.registers.create(date: '2012-02-01', description: 'Gas', amount: 250, 
-                        balance_type: 'Outgoing')
+                         balance_type: 'Outgoing')
 
   admin.registers.create(date: '2012-02-01', description: 'Food', amount: 200, 
-                        balance_type: 'Outgoing')
+                         balance_type: 'Outgoing')
   
   admin.registers.create(date: '2012-02-01', description: 'Subway', amount: 140, 
-                        balance_type: 'Outgoing')
+                         balance_type: 'Outgoing')
   
   admin.registers.create(date: '2012-02-01', description: 'Income', amount: 3000, 
-                        balance_type: 'Incoming')
+                         balance_type: 'Incoming')
   
   admin.registers.create(date: '2012-02-01', description: 'New dog!', amount: 1500, 
-                        balance_type: 'Outgoing')
+                         balance_type: 'Outgoing')
 
   # 2013
   admin.registers.create(date: '2013-02-01', description: 'Rent', amount: 1500, 
-                        balance_type: 'Outgoing')
+                         balance_type: 'Outgoing')
   
   admin.registers.create(date: '2013-02-01', description: 'Gas', amount: 250, 
-                        balance_type: 'Outgoing')
+                         balance_type: 'Outgoing')
 
   admin.registers.create(date: '2013-02-01', description: 'Food', amount: 200, 
-                        balance_type: 'Outgoing')
+                         balance_type: 'Outgoing')
   
   admin.registers.create(date: '2013-02-01', description: 'Subway', amount: 140, 
-                        balance_type: 'Outgoing')
+                         balance_type: 'Outgoing')
   
   admin.registers.create(date: '2013-02-01', description: 'Income', amount: 3000, 
-                        balance_type: 'Incoming')
+                         balance_type: 'Incoming')
   
   admin.registers.create(date: '2013-02-01', description: 'New dog!', amount: 1500, 
-                        balance_type: 'Outgoing')
+                         balance_type: 'Outgoing')
 
 end
 
