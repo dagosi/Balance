861d14566c2bbd6a7d85a3c1f3259b8a3914e00d
Daniel Gomez Sierra <danielgomezsierra@gmail.com>
2013-09-10 18:35:08 -0400

    Added code climate

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Balance::Application.routes.draw do
   resources :registers do
     match 'destroy_month/:year/:month' => 'registers#destroy_month', as: :destroy_month, via: :delete, on: :collection
     match 'destroy_year/:year' => 'registers#destroy_year', as: :destroy_year, via: :delete, on: :collection
-
   end
   
   # Show the registers summary.
